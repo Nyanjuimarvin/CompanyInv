91adc120cf67e54eff2a1737ee1366b7547b1d89
Nyanjuimarvin <marnyanjui@gmail.com>
2022-01-26 10:03:51 +0300

    Get user input in typescript

--- public/app.js
+++ public/app.js
@@ -1,8 +1,5 @@
 import { Clientowes } from "./classes/sell.js";
 import { Oweclient } from "./classes/buy.js";
-let invOne;
-invOne = new Clientowes("ergounand", "rtx", 90);
-console.log(invOne);
 //Add ! to indicate object is not null
 const form = document.querySelector('form');
 //input elements
@@ -14,3 +11,16 @@ const amount = document.querySelector('#amount');
 const client1 = new Clientowes("Duke", "Blowing up", 45);
 const client2 = new Oweclient("Duke", "Blowing up", 45);
 console.log(client1, client2);
+//Try testing Dom manipulation
+//specify e is of type event
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let testOne;
+    if (transaction.value === "buy") {
+        testOne = new Oweclient(client.value, items.value, amount.valueAsNumber);
+    }
+    else {
+        testOne = new Clientowes(client.value, items.value, amount.valueAsNumber);
+    }
+    console.log(testOne);
+});
--- src/app.ts
+++ src/app.ts
@@ -3,21 +3,31 @@ import { Oweclient } from "./classes/buy.js";
 import { StringReturn } from "./interfaces/StringReturn.js";
 
 
-
-let invOne:StringReturn;
-invOne = new Clientowes("ergounand","rtx",90);
-console.log(invOne);
 //Add ! to indicate object is not null
 const form = document.querySelector('form')!;
 
 //input elements
 const transaction = document.querySelector('#transaction') as HTMLInputElement;
 const client = document.querySelector("#tofrom") as HTMLInputElement;
-const items = document.querySelector('#items')as HTMLInputElement;
+const items = document.querySelector('#items') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
 
 //Try creating a new class instance
-const client1 = new Clientowes("Duke","Blowing up",45);
-const client2 = new Oweclient("Duke","Blowing up",45);
-console.log(client1,client2);
+const client1 = new Clientowes("Duke", "Blowing up", 45);
+const client2 = new Oweclient("Duke", "Blowing up", 45);
+console.log(client1, client2);
+
+//Try testing Dom manipulation
+//specify e is of type event
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+    let testOne: StringReturn;
+    if (transaction.value === "buy") {
+        testOne = new Oweclient(client.value, items.value, amount.valueAsNumber);
+    } else {
+        testOne = new Clientowes(client.value, items.value, amount.valueAsNumber);
+    }
+
+    console.log(testOne);
+});
